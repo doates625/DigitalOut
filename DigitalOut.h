2b552fdad131bf66a144e489eb5f40ade6ff5318
Dan <doates625@gmail.com>
2019-10-18 12:04:20 -0700

    Moved description to README

--- DigitalOut.h
+++ DigitalOut.h
@@ -2,15 +2,6 @@
  * @file DigitalOut.h
  * @brief Class for Arduino and Mbed digital outputs
  * @author Dan Oates (WPI Class of 2020)
- * 
- * This class implements the Mbed DigitalOut class for Arduino to allow for
- * cross-platform libraries.
- * 
- * Dependencies:
- * - Platform: https://github.com/doates625/Platform.git
- * 
- * References:
- * - Mbed DigitalOut: https://os.mbed.com/docs/mbed-os/v5.14/apis/digitalout.html
  */
 #pragma once
 #include <Platform.h>
---
+++ README.md
@@ -0,0 +1,12 @@
+# DigitalOut
+Class for Arduino and Mbed digital outputs  
+Written by Dan Oates (WPI Class of 2020)
+
+### Description
+This class implements the Mbed DigitalOut class for Arduino to allow fo cross-platform libraries.
+
+### Dependencies
+- [Platform](https://github.com/doates625/Platform.git)
+
+### References
+- [Mbed DigitalOut](https://os.mbed.com/docs/mbed-os/v5.14/apis/digitalout.html)
